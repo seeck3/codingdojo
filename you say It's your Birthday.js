55335a9dd407e76188e0ad3ba06c0c88d60f1eff
seeck3 <39784408+seeck3@users.noreply.github.com>
2018-06-05 18:45:55 -0400

    Add files via upload

---
+++ you say It's your Birthday.js
@@ -0,0 +1,14 @@
+
+var Birthday = (4, 19);
+function yourBirthday(Birthday){
+    
+    if(Birthday == (4, 19)){
+
+        console.log("How did you know?")
+    }else{
+        console.log("Just another day...")
+    }
+
+}
+
+yourBirthday(Birthday);
