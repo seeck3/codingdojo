5e68bc27648f0fe448f4a4f095b50889202908ec
seeck3 <39784408+seeck3@users.noreply.github.com>
2018-06-05 19:20:48 -0400

    Add files via upload

---
+++ Counting, the Dojo Way.js
@@ -0,0 +1,18 @@
+
+
+function dojoWay(){
+
+    for(var i = 0; i <= 100; i++){
+
+        if(i % 10 === 0){
+            console.log("Dojo " + i) // I added i, to check if it is working properly
+        }else if(i % 5 === 0){
+            console.log("Coding " + i) // i added i, to check if it is working properly
+        }
+    }
+    return;
+
+}
+
+dojoWay();
+
