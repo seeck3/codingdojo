2902a4c3e6be9fca9bf1f9d98f3c0fe1f33f1406
seeck3 <39784408+seeck3@users.noreply.github.com>
2018-06-05 18:23:19 -0400

    Add files via upload

---
+++ Multiples of Three - But Not All.js
@@ -0,0 +1,21 @@
+
+
+function multiThree(){
+    var arr = [];
+    
+    for(var i = -300; i < 0; i++){
+        if(i % 3 === 0){
+
+            if(i !== -3){
+                arr.push[i];
+               
+            }else if(i !== -6){
+                arr.push[i];
+            }                    
+        }      
+    }
+    return arr;
+
+}
+
+multiThree(); 
