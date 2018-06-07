ed8aca97d9498ded06092306b53195e29a88f4ae
seeck3 <39784408+seeck3@users.noreply.github.com>
2018-06-06 21:21:58 -0400

    Add files via upload

---
+++ What Do You Know.js
@@ -0,0 +1,13 @@
+
+
+function doorLock(incoming){
+
+    if(incoming == "Marco"){
+
+        console.log("Welcome, Master Marco Seo");
+    }else{
+        console.log("I don't know who you are.. I will find you, and I will kill you");
+    }
+}
+
+doorLock("Marco");
